8e088b710b6ab5e1858c739e47fcccd458c2347e
Sj-407 <111113371+Sj-407@users.noreply.github.com>
2022-11-07 10:36:21 +0530

    Add files via upload

---
+++ Shruti_DSA_LabDSA/DSA-Balanced Bracket/src/com/gl/javafsd/dsa/balancedbrackets/BalancedBracketsCheckerTest.java
@@ -0,0 +1,28 @@
+package com.gl.javafsd.dsa.balancedbrackets;
+
+public class BalancedBracketsCheckerTest {
+	
+	public static void main(String[] args) {
+		
+		test("");
+		test("([[{}]])");	
+	}
+	
+	static void test(String brackets) {
+		
+		BalancedBracketsChecker checker = new BalancedBracketsChecker(brackets);
+		
+		boolean result = checker.check();
+		if (result) {
+			System.out.println("Bracket expression " + brackets + " is balanced");
+		}else {
+			System.out.println("Bracket expression " + brackets + " is NOT balanced");			
+		}		
+	}
+}
+	
+	
+	
+	
+
+
